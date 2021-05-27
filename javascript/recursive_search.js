8083437eb9df0f9a98add0ced04656d54e799b27
OM Miles <miles@ommiles.com>
2021-05-26 17:28:25 -0700

    algos complete

--- javascript/recursive_search.js
+++ javascript/recursive_search.js
@@ -1,5 +1,10 @@
 function recursiveSearch(arr, target) {
-  // type your code here
+  if (arr[0] == target) {
+    return true
+  } else if (arr.length == 0) {
+    return false
+  }
+  return recursiveSearch(arr.slice(1), target)
 }
 
 if (require.main === module) {
@@ -17,3 +22,11 @@ module.exports = recursiveSearch;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// iterate through each index of arr
+// if the target val is found or not
+// return an appropriate boolean
+// BASE CASE: arr.length if falsy
+// BASE CASE: truthy if val = target
+// arr = arr[0]
+// pass a shortened array
